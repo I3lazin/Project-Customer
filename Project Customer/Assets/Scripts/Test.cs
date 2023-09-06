178f72ef679837071863570f95bdcd286146f58d
I3lazin <td.akkerman@hotmail.com>
2023-09-06 12:21:31 +0200

    test branch

--- Project Customer/Assets/Scripts/Test.cs
+++ Project Customer/Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
