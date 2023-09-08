4f367e8cde5ca2d1f52d2c44a1159ac6236e2d6d
I3lazin <td.akkerman@hotmail.com>
2023-09-08 10:26:04 +0200

    small changes

--- Project Customer/Assets/Scripts/TriggerCheck.cs
+++ Project Customer/Assets/Scripts/TriggerCheck.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerCheck : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class TriggerCheck : MonoBehaviour
         if (other.tag == "collectable")
         {
             other.GetComponent<Collectable>().Collect();
+        } 
+        if (other.tag == "End")
+        {
+            SceneManager.LoadScene("Dasha");
         }
     }
 }
